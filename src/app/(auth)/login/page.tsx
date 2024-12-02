6a38f54b09f4e5026e2ea9cefb41834dadac3b93
Emma <Emma@fanap.ir>
2024-12-02 19:13:19 +0330

    add login server action

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
+import { login } from "./_actions/login";
 
 export default function LoginPage() {
   return (
     <div>
       <h1 className="mb-4 text-center text-xl">Login</h1>
-      <form className="space-y-6">
+      <form action={login} className="space-y-6">
         <div className="flex flex-col gap-1">
           <label htmlFor="email">Email</label>
-          <input id="email" name="email" type="email" />
+          <input id="email" name="email" type="email" required />
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="password">Password</label>
-          <input id="password" name="password" type="password" />
+          <input id="password" name="password" type="password" required />
         </div>
         <button className="w-full bg-black px-3 py-2 text-white">Login</button>
         <p>
