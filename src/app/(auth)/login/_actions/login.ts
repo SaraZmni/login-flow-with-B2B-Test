ac987e4fa56a43103f77f3b4c420a70282b307c9
Emma <Emma@fanap.ir>
2024-12-03 14:05:46 +0330

    add validation to client and server login component

--- src/app/(auth)/login/_actions/login.ts
+++ src/app/(auth)/login/_actions/login.ts
@@ -1,5 +1,25 @@
 'use server';
+import { z } from 'zod';
 
-export async function login(formData: FormData) {
+
+const schema = z.object({
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+  })
+
+
+  export async function login(currentState: any, formData: FormData) {
     console.log('login', formData);
+
+    const data = schema.safeParse({
+        email: formData.get('email'),
+        password: formData.get('password'),
+      });
+    
+      // Return early if the form data is invalid
+      if (!data.success) {
+        return {
+          errors: data.error.flatten().fieldErrors,
+        };
+      }
   }
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,18 +1,23 @@
+"use client";
+import { useFormState } from "react-dom";
 import Link from "next/link";
 import { login } from "./_actions/login";
 
 export default function LoginPage() {
+  const [state, formAction] = useFormState(login, null);
   return (
     <div>
       <h1 className="mb-4 text-center text-xl">Login</h1>
-      <form action={login} className="space-y-6">
+      <form action={formAction} className="space-y-6">
         <div className="flex flex-col gap-1">
           <label htmlFor="email">Email</label>
           <input id="email" name="email" type="email" required />
+          <p aria-live="polite">{state?.errors.email}</p>
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="password">Password</label>
           <input id="password" name="password" type="password" required />
+          <p aria-live="polite">{state?.errors.password}</p>
         </div>
         <button className="w-full bg-black px-3 py-2 text-white">Login</button>
         <p>
